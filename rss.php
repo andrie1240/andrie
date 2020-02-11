f8d10d6d589ae3eb55485a8f1db33ec8fdf70210
andrie1240 <55270566+andrie1240@users.noreply.github.com>
2020-02-11 17:35:43 +0800

    Add files via upload

---
+++ rss.php
@@ -0,0 +1,19 @@
+<?php
+$domOBJ = new DOMDocument();
+$domOBJ->load("https://app-xml.herokuapp.com/");
+
+$content = $domOBJ->getElementsByTagName("item");
+
+foreach ($content as $data) {
+
+    $id = $data->getElementsByTagName("id")->item(0)->nodeValue;
+    $username = $data->getElementsByTagName("username")->item(0)->nodeValue;
+    $password = $data->getElementsByTagName("password")->item(0)->nodeValue;
+
+    echo "
+    <ul>
+    <li>Id: <strong>$id</strong></li>
+    <li>Username: <strong>$username</strong></li>
+    <li>Password: <strong>$password</strong></li>
+    </ul>";
+}
